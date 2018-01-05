9a3a3a31d8a13b90d7ae8cc35d58ad3dd2ab3a17
Jack Luo <jackluo94@hotmail.com>
2018-01-04 16:57:39 -0800

    added a worknig chat
    fsdfsdsdg

--- index.php
+++ index.php
@@ -1,50 +1,9 @@
 
 
-
-
-
-
-
 <?php
 
 $page_title = "Home";
 require "templates/header.php";
-
-
-?>
-
-<h1>AJAX? Wish it worked.</h1>
-<form action='index.php' method='post' class='ajaxform'>
-    <input type='text' name='txt' value='Test Text'>
-    <input type='submit' value='submit'>
-</form>
-
-<div id='result'>Result comes here..</div>
-
-<?php
-// All form data is in $_POST
-
-// Now perform actions on form data here and
-// create an result array something like this
-$arr = array( 'result' => 'This is my result' );
-echo json_encode( $arr );
-?>
-
-
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-
-<?php
-
-
 
 
 $python = "python";
